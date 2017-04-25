1441093cb7bf47e53c435b23f8a5e22ea0a717bb
PabloMacLinares <pablomaclinares@gmail.com>
2017-04-25 22:01:44 +0200

    25/04/2017

--- app/src/main/java/com/dam2/clickneat/preferences/Preferences.java
+++ app/src/main/java/com/dam2/clickneat/preferences/Preferences.java
@@ -4,13 +4,23 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import java.util.Map;
+
 /**
  * Created by Pablo on 25/04/2017.
+ */
+
+/**
+ * Cada clase que necesite usar Preferencias deberia crear su propio Handler
  */
 
 public class Preferences {
 
     public static final int DEFAULT_INTEGER = Integer.MIN_VALUE;
+    public static final float DEFAULT_FLOAT = Float.MIN_VALUE;
+    public static final long DEFAULT_LONG = Long.MIN_VALUE;
+    public static final boolean DEFAULT_BOOLEAN = Boolean.FALSE;
+    public static final String DEFAULT_STRING = "";
 
     SharedPreferences sharedPreferences;
     SharedPreferences.Editor editor;
@@ -27,5 +37,53 @@ public class Preferences {
 
     public int getInteger(String key){
         return sharedPreferences.getInt(key, DEFAULT_INTEGER);
+    }
+
+    public void setFloat(String key, float value){
+        editor.putFloat(key, value);
+        editor.commit();
+    }
+
+    public float getFloat(String key){
+        return sharedPreferences.getFloat(key, DEFAULT_FLOAT);
+    }
+
+    public void setLong(String key, long value){
+        editor.putLong(key, value);
+        editor.commit();
+    }
+
+    public long getLong(String key){
+        return sharedPreferences.getLong(key, DEFAULT_LONG);
+    }
+
+    public void setBoolean(String key, boolean value){
+        editor.putBoolean(key, value);
+        editor.commit();
+    }
+
+    public boolean getBoolean(String key){
+        return sharedPreferences.getBoolean(key, DEFAULT_BOOLEAN);
+    }
+
+    public void setString(String key, String value){
+        editor.putString(key, value);
+        editor.commit();
+    }
+
+    public String getString(String key){
+        return sharedPreferences.getString(key, DEFAULT_STRING);
+    }
+
+    public Map<String, ?> getAll(){
+        return sharedPreferences.getAll();
+    }
+
+    /**
+     * Esta función puede ser peligrosa, evitar su uso D:
+     */
+    public void clearAll(){
+        editor.clear();
+        editor.commit();
     }
 }
