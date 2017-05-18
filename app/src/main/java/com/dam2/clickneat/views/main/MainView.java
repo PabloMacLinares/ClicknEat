557a6310a931434f35976f388ec820dc650e126e
grupo1damizv <grupo1damizv@gmail.com>
2017-05-18 21:52:19 +0200

    layout publicacion crear/editar

--- app/src/main/java/com/dam2/clickneat/views/main/MainView.java
+++ app/src/main/java/com/dam2/clickneat/views/main/MainView.java
@@ -1,22 +1,14 @@
 package com.dam2.clickneat.views.main;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
 
 import com.dam2.clickneat.R;
 import com.dam2.clickneat.views.BaseActivity;
-import com.dam2.clickneat.views.chats.ChatsView;
 
 public class MainView extends BaseActivity
         implements MainContract.View, NavigationView.OnNavigationItemSelectedListener {
@@ -26,8 +18,8 @@ public class MainView extends BaseActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_publicacion_crear_editar);
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -48,7 +40,7 @@ public class MainView extends BaseActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        presenter = new MainPresenter(this);
+        presenter = new MainPresenter(this);*/
 
     }
 
